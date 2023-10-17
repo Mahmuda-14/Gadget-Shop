e738ee6eccf271eae2b9499623c5045dde3d4d7a
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2023-10-17 22:05:01 +0600

    Creating component folder

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLk6-G6CMIW0IB6VjKynMmWvxJVgaOOJ4",
+  authDomain: "brand-ass-10.firebaseapp.com",
+  projectId: "brand-ass-10",
+  storageBucket: "brand-ass-10.appspot.com",
+  messagingSenderId: "186431319359",
+  appId: "1:186431319359:web:4bebfb78a3d8fc9674378a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
